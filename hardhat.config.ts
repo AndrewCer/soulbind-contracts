1d40635fcde727c8d339e0888b57c06762678b3d
AndrewCer <cer.andrew@gmail.com>
2023-01-21 09:57:33 -0700

    update hardhat config with mainnet deploy rpc url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,12 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   networks: {
-    matic: {
+    polygon_mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.SIGNER_KEY || '']
+    },
+    polygon_main_net: {
+      url: "https://polygon-rpc.com",
       accounts: [process.env.SIGNER_KEY || '']
     }
   },
@@ -20,6 +24,9 @@ const config: HardhatUserConfig = {
       }
     }
   },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_KEY,
+ }
 };
 
 export default config;
