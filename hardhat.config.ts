d894fadaf32c2a5efd8f738ea0e1151dbe9ea209
AndrewCer <cer.andrew@gmail.com>
2023-02-28 17:30:10 -0700

    add support for base (coinbase L2) testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,10 +6,22 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   networks: {
+    // Base testnet
+    base_goerli: {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    // Base local
+    base_local: {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    // Polygon testnet
     polygon_mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.SIGNER_KEY || '']
     },
+    // Polygon mainnet
     polygon_main_net: {
       url: "https://polygon-rpc.com",
       accounts: [process.env.SIGNER_KEY || '']
@@ -25,8 +37,30 @@ const config: HardhatUserConfig = {
     }
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_KEY,
- }
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_KEY!,
+      polygon: process.env.POLYGONSCAN_KEY!,
+      // No api key for basescan - fill in with random value
+      'base-goerli': '12345'
+    },
+    customChains: [
+      {
+        network: "base-goerli",
+        chainId: 84531,
+        urls: {
+          // Pick a block explorer and uncomment those lines
+
+          // Blockscout
+          // apiURL: "https://base-goerli.blockscout.com/api",
+          // browserURL: "https://base-goerli.blockscout.com"
+
+          // Basescan by Etherscan
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org"
+        }
+      }
+    ]
+  }
 };
 
 export default config;
